0bd775cc95863423afc71e03c4ab294eaa2bdd2e
R221 <rcjames1111@gmail.com>
2022-02-01 21:30:22 +0900

    Added Cart, React Router, Cart functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 
 import { commerce } from "./lib/commerce";
-import { Products, Navbar } from "./components";
+import { Products, Navbar, Cart } from "./components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -12,17 +14,39 @@ const App = () => {
     setProducts(data);
   };
 
+  // Getting the cart and setting its state
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
-  console.log(products);
+  console.log(cart);
 
+  //Wrap products in cart in a router so we can navigate back and forth
+  //Products is the root path, "/". If we're in cart, render cart
   return (
-    <div>
-      <Navbar />
-      <Products products={products} />
-    </div>
+    <Router>
+      <Navbar totalItems={cart.total_items} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Products products={products} onAddToCart={handleAddToCart} />
+          }
+        />
+        <Route path="/cart" element={<Cart cart={cart} />} />
+      </Routes>
+    </Router>
   );
 };
 
